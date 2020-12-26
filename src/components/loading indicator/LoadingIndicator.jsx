53d27811210f42c2e5d51f8cff99fee7ed4fa550
BlackH3art <Jaroslaw.Musielak@outlook.com>
2020-12-26 16:49:27 +0100

    LoadingIndicator component

---
+++ src/components/loading indicator/LoadingIndicator.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+import './styles/loading_indicator.css'
+
+
+function LoadingIndicator() {
+
+  return (
+    <>
+      <div className="loadingio-spinner-spinner-hb5mfd08m5h">
+        <div className="ldio-0up835qgtc4p">
+          <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default LoadingIndicator;
